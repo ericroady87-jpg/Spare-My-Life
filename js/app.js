70d236a289cdef7c0946cb54c9bcf4e6310e6478
ERoady87 <ericroady87@gmail.com>
2025-10-26 21:01:10 -0400

    Spare-My-Life WIP

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 /*-------------- Constants -------------*/
-
+const wordDisplay = document.getElementById("word-display");
+const alphabetButtons = document.getElementById("alphabet-buttons");
+const messageDisplay = document.getElementById("message");
+const resetButton = document.getElementById("reset-button");
+const hangmanFigure = document.getElementById("hangman-figure");
 
 /*---------- Variables (state) ---------*/
-
+let chosenWord = '';
+let guessedLetters = [];
+let remainingGuesses = 6;
 
 /*----- Cached Element References  -----*/
 
